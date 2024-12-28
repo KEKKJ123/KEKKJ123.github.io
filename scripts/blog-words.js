39a92e312a94aa0906b6115c01ecfd4f0429fc19
KEKKJ123 <2899851191@qq.com>
2024-12-28 17:19:42 +0800

    Site updated: 2024-12-28 17:19:42

--- scripts/blog-words.js
+++ scripts/blog-words.js
@@ -1,3 +1,3 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
-    injects.bodyEnd.file('showword', 'source/scripts/show_wordcount_total.js', { key: 'value' }, -1);
+    injects.bodyEnd.file('showword', 'source/scripts/show_wordcount_total.ejs', { key: 'value' }, -1);
   });
